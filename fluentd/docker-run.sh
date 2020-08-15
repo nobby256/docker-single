bfd12d5770bbe32cb58ba314c98b32a2727aea00
nobby256 <helloworld_1024@yahoo.co.jp>
2020-08-16 00:46:56 +0900

    更新

--- fluentd/docker-run.sh
+++ fluentd/docker-run.sh
@@ -1,1 +1,1 @@
-docker run -it -p 24224:24224 -v [test.conf‚Ö‚Ìâ‘ÎƒpƒX]:/fluentd/etc/test.conf -e FLUENTD_CONF=test.conf fluent/fluentd:latest
+docker run -it -p 24224:24224 -v $(/opt/docker-single/fluentd)/mount/test.conf:/fluentd/etc/test.conf -e FLUENTD_CONF=test.conf fluent/fluentd:latest
